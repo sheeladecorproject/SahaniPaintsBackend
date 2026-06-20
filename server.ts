db48bbcb4822bacb2b02585f2959540fe1f362db
ShashankPhiskeKanaka <shashank.phiske@kanakasoftware.com>
2026-06-20 23:31:34 +0530

    clean build

--- server.ts
+++ server.ts
@@ -32,7 +32,7 @@ const stream = {
 };
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', "https://sahanipaints.netlify.app/"],
+    origin: ['http://localhost:5173', 'http://localhost:5174', "https://sahanipaints.netlify.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
